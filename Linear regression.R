c15ec48d4e1c5e0c9068ce85e4081d12a6935b8d
sjamar <104518071+sjamar@users.noreply.github.com>
2022-06-08 22:54:40 +0300

    multiple regression

--- Linear regression.R
+++ Linear regression.R
@@ -12,3 +12,10 @@ plot(Latitude,annual,
 mod <- lm(annual~Latitude)
 abline(mod) # adding the linear rgression line
 abline(mod,col=2,lwd=3) # adding color and thickness
+
+multiple_regression <- temp_warm_DATA %>% 
+  select(annual,Latitude,Longitude)
+plot(multiple_regression,main="Multiple linear regression")
+
+multiple_linear_regression <- lm(annual~Latitude+Longitude,data=multiple_regression)
+summary(multiple_linear_regression)
